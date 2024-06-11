685cf21043277e771ec6b75be0d4e9d27d130a6f
kimseula <kimseula20@gmail.com>
2024-06-11 22:07:55 +0900

    BE02 advance 문제 풀이 추가

---
+++ src/Be02Team/meeting_20240608/메소드/Advance/advance.java
@@ -0,0 +1,10 @@
+package Be02Team.meeting_20240608.메소드.Advance;
+
+public class advance {
+
+  public static void main(String[] args) {
+    RockScissPaper rsp = new RockScissPaper();
+    rsp.start();
+  }
+
+}
