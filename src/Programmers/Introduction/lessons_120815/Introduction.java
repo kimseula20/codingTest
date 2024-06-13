3dc0b29d8398b474111b9f974392b358ae6863bf
kimseula <kimseula20@gmail.com>
2024-06-14 00:00:04 +0900

    코테 입문 : 120815 추가

---
+++ src/Programmers/Introduction/lessons_120815/Introduction.java
@@ -0,0 +1,28 @@
+package Programmers.Introduction.lessons_120815;
+
+public class Introduction {
+
+  public static void main(String[] args) {
+
+    int n = 10;
+    int answer = solution(n);
+    System.out.println(answer);
+  }
+
+  public static int solution(int n) {
+    int max = Math.max(n, 6); // a
+    int min = Math.min(n, 6); // b
+
+    // 최대 공약수
+    while (min != 0) {  // c
+      int r = max % min;
+      max = min;
+      min = r;
+    }
+
+    //최소 공배수 = 두수의 곱 / 최대공약수
+    return (n * 6 / max)/6;
+  }
+}
+
+
