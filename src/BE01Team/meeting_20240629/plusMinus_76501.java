3ba5e6653a2c168d2a8b496d34279df38883aa5b
kimseula <kimseula20@gmail.com>
2024-06-26 21:42:37 +0900

    음양더하기 문제 추가(30일 회의 공통문제)

---
+++ src/BE01Team/meeting_20240629/plusMinus_76501.java
@@ -0,0 +1,28 @@
+package BE01Team.meeting_20240629;
+
+public class plusMinus_76501 {
+
+  public static void main(String[] args) {
+
+    int[] absolutes = {4,7,12};
+    boolean[] signs = {true,false,true};
+
+    int answer = solution(absolutes, signs);
+    System.out.println(answer);
+
+  }
+
+  public static int solution(int[] absolutes, boolean[] signs)
+  {
+    int answer = 0;
+    for(int i = 0; i < absolutes.length; i++) {
+      if(signs[i]) {
+        answer += absolutes[i];
+      } else {
+        answer -= absolutes[i];
+      }
+    }
+    return answer;
+  }
+
+}
